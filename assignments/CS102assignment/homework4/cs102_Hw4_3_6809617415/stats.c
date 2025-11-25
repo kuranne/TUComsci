467d0009302534970160440578cf7e00831fb6a0
kuranne <kurannemaisa@outlook.com>
2025-11-25 16:27:00 +0700

    Fix: CS102 hw4-3

--- assignments/CS102assignment/homework4/cs102_Hw4_3_6809617415/stats.c
+++ assignments/CS102assignment/homework4/cs102_Hw4_3_6809617415/stats.c
@@ -1,6 +1,22 @@
 #include "stats.h"
 #include <stdlib.h>
 
+void swap(int* arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void bubbleSort(int arr[], int N) {
+    int i, j;
+	for (i=0;i<N-1;i++) {
+        for (j=0;j<N-i-1;j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr, j, j + 1);
+        }
+    }
+}
+
 void stats(int *data, int N, struct statistics *stats) {
     int i;
     
